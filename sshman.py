6d3ea0f1c77b3346d457900c07308e459133901c
philippe ouellette <philippeouellette@protonmail.com>
2020-10-18 11:14:40 -0400

    Initial commit

---
+++ sshman.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import json,os
+os.system('clear')
+
+def main_menu():
+    """Main menu function. Simply asks a question that determines the next action for the program."""
+    return input("Do you wanna 1) add an ip address or 2) use an existing one")
+
+def new_address():
+    """Here, we get a new sessions that we save in sessions.json 
+    file. We also copy our public ssh key to the remote device."""
+
+
+def existing_address():
+    """we fetch existing sessions from json file and display them. 
+    Gotta let the user choose which session he wants to use but also let him go back to the main menu."""
+    with open('sessions.json') as file:
+        data = json.load(file)
+
+    for session in data['sessions']:
+        print(session['username'])
+
+def main():
+    """main function"""
+
+
+if __name__=="__main__":
+    main()
