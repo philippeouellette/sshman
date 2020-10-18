4f15a878bd076046e8483a2c39683751469120ef
oli <fuckyouphil@trump2020.com>
2020-10-18 13:49:10 -0700

    test2

--- sshman.py
+++ sshman.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import json,os, getpass
+
 os.system('clear')
 
 def main_menu():
     """Main menu function. Simply asks a question that determines the next action for the program."""
     return input("Do you wanna 1) add an ip address or 2) use an existing one")
+
 
 def new_address():
     """Here, we get a new session that we save in sessions.json 
@@ -16,19 +18,17 @@ def existing_address():
     """we fetch existing sessions from json file and display them. 
     Gotta let the user choose which session he wants to use but also let him go back to the main menu."""
 
-
-    #   asdsdasdas ads asd 
-
-
     with open('sessions.json') as file:
         data = json.load(file)
 
     for session in data['sessions']:
         print(session['username'])
 
+
 def main():
     """main function"""
     print(new_address())
 
-if __name__=="__main__":
+
+if __name__== "__main__":
     main()
