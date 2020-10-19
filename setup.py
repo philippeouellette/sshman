c18161b75b2e38af1552818c117260ada5a72fa5
philippe ouellette <philippeouellette@protonmail.com>
2020-10-19 17:38:41 -0400

    test

--- setup.py
+++ setup.py
@@ -6,25 +6,21 @@ except ImportError:
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-config = {
-    "keywords": "ssh sessions manager",
-    "author": "Philippe Ouellette & Olivier Granger-Hotte",
-    "description": "A simple ssh sessions manager",
-    "long_description": long_description,
-    "long_description_content_type": "text/markdown",
-    "url": "http://192.168.100.23:3000/ouellp/sshman",
-    "download_url": "http://192.168.100.23:3000/ouellp/sshman",
-    "author_email": "philippeouellette@protonmail.com",
-    "version": "2.0.5",
-    "install_requires": ["bullet"],
-    "packages": ["sshman"],
-    "entry_points": {
-        "console_scripts": ['sshman = sshman.__main__:main']
-    },
-    "name": "sshman",
-    "classifiers": [
-        "Programming Language :: Python :: 3.8",
-    ],
-}
-
-setup(**config)
+setup(
+    name = 'sshman',
+    keywords="ssh sessions manager",
+    author="Philippe Ouellette & Olivier Granger-Hotte",
+    description="A simple ssh sessions manager",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://192.168.100.23:3000/ouellp/sshman",
+    download_url="http://192.168.100.23:3000/ouellp/sshman",
+    author_email="philippeouellette@protonmail.com",
+    version="2.0.5",
+    install_requires=["bullet"],
+    packages=["sshman"],
+    entry_points={"console_scripts": ['sshman = sshman.__main__:main']},
+    data_files=[('sshman',['sshman/*.json'])],
+    include_package_data=True,
+    classifiers=["Programming Language :: Python :: 3.8",]
+)
